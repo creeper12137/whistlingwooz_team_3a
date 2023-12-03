a5d051786a58594024914e0d1077f694265ade2b
mypri <myprinciples40@gmail.com>
2023-12-03 11:03:42 +1100

    [EAP-57] Wedding page - added
    Add wedding page design
    
    Other:
    1. Correct misspellings of survey_form_screen
    2. Add unique_code_screen for connecting with survey_form

---
+++ whistlingwoodz/lib/screens/survey_form_screen.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:whistlingwoodz/widgets/app_bar_widget.dart';
+import 'package:whistlingwoodz/widgets/bottom_navigation_bar_widget.dart';
+
+class SurveyForm extends StatefulWidget {
+  const SurveyForm({super.key});
+
+  @override
+  State<SurveyForm> createState() => _SurveyFormState();
+}
+
+class _SurveyFormState extends State<SurveyForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const AppBarPage(),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: RadialGradient(
+            colors: [
+              Color.fromARGB(255, 139, 46, 43),
+              Color(0xff510400),
+            ],
+          ),
+        ),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Image.asset(
+                  'assets/images/mandalaTop.png',
+                  width: 300,
+                ),
+                const Text(
+                  'This is for the Survery form view',
+                  style: TextStyle(
+                    color: Color(0xffFFD700),
+                    fontSize: 15,
+                    fontStyle: FontStyle.italic,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Image.asset(
+                  'assets/images/mandala.png',
+                  width: 700,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      bottomNavigationBar: const BottomBar(),
+    );
+  }
+}
