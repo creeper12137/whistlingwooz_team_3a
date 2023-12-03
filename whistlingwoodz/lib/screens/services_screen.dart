dbdf44bed02f621e1da098184bae60a21ecb478b
Simone88kaur <143977991+Simone88kaur@users.noreply.github.com>
2023-12-03 13:40:25 +1100

    Update services_screen.dart

--- whistlingwoodz/lib/screens/services_screen.dart
+++ whistlingwoodz/lib/screens/services_screen.dart
@@ -1,60 +1,92 @@
 import 'package:flutter/material.dart';
-import 'package:whistlingwoodz/widgets/app_bar_widget.dart';
-import 'package:whistlingwoodz/widgets/bottom_navigation_bar_widget.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-class Services extends StatefulWidget {
-  const Services({super.key});
+class ServicesScreen extends StatelessWidget {
+  final List<ServiceModel> services = [
+    ServiceModel(
+      title: "Understanding the Event Genre",
+      imageUrl:"assets/simage1.png",
+    ),
+    ServiceModel(
+      title: "Venue Design and Production",
+      imageUrl:"assets/vimage2.png",
+    ),
+    ServiceModel(
+      title: "Event Planning",
+      imageUrl:"assets/evimage4.png",
+    ),
+    ServiceModel(
+      title: "Entertainment & Performances",
+      imageUrl:"assets/perimage5.png",
+    ),
+    ServiceModel(
+      title: "Commute & Cabs",
+      imageUrl:"assets/ccimage6.png",
+    ),
+    ServiceModel(
+      title: "Vendors Range",
+      imageUrl:"assets/venimage7.png",
+    ),
+  ];
 
-  @override
-  State<Services> createState() => _ServicesState();
-}
-
-class _ServicesState extends State<Services> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const AppBarPage(),
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: RadialGradient(
-            colors: [
-              Color.fromARGB(255, 139, 46, 43),
-              Color(0xff510400),
-            ],
-          ),
-        ),
-        child: Center(
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Image.asset(
-                  'assets/images/mandalaTop.png',
-                  width: 300,
-                ),
-                const Text(
-                  'This is the Service view',
-                  style: TextStyle(
-                    color: Color(0xffFFD700),
-                    fontSize: 15,
-                    fontStyle: FontStyle.italic,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Image.asset(
-                  'assets/images/mandala.png',
-                  width: 700,
-                ),
-              ],
-            ),
-          ),
-        ),
+      appBar: AppBar(
+        title: Text('Services'),
       ),
-      bottomNavigationBar: const BottomBar(),
+      body: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 8.0,
+          mainAxisSpacing: 8.0,
+        ),
+        itemCount: services.length,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () {
+              _launchURL();
+            },
+            child: Card(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Expanded(
+                    child: Image.asset(
+                      services[index].imageUrl,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      services[index].title,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 16.0),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
+
+  _launchURL() async {
+    const url = 'https://whistlingwoodz.com.au/contact-us';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+}
+
+class ServiceModel {
+  final String title;
+  final String imageUrl;
+
+  ServiceModel({required this.title, required this.imageUrl});
 }
