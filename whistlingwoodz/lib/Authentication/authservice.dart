13ccce63346ff241a7c10bfe41dbc70d939f5d86
Simone88kaur <143977991+Simone88kaur@users.noreply.github.com>
2023-12-05 12:55:23 +1100

    Update authservice.dart

--- whistlingwoodz/lib/Authentication/authservice.dart
+++ whistlingwoodz/lib/Authentication/authservice.dart
@@ -1,1 +1,26 @@
+// lib/services/AuthService.dart
+
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<User?> signIn(String email, String password) async {
+    try {
+      UserCredential result = await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      User? user = result.user;
+      return user;
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  Future<void> signOut() async {
+    await _auth.signOut();
+  }
+}
 
