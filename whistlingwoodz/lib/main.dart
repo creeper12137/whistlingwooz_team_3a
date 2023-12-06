07c8c4bdcf8a0787355eda8284b1c3740c004cb0
brandonlsy23 <brandonlsy616@gmail.com>
2023-12-06 09:33:57 +0400

    FirebaseAuth

--- whistlingwoodz/lib/main.dart
+++ whistlingwoodz/lib/main.dart
@@ -1,22 +1,49 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:whistlingwoodz/screens/landing_page_screen.dart';
+import 'package:whistlingwoodz/screens/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
-  runApp(const MyApp());
-}
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp( MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Whistlingwoodz',
       theme: ThemeData(
         primarySwatch: Colors.amber,
       ),
-      home: const LandingPage(),
+      home: const MyApp(),
+    )
     );
-  }
+ 
 }
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  
+  @override
+  Widget build(BuildContext context)  {  
+ 
+
+    return  StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot){
+        if(snapshot.hasData){
+          debugPrint('successful logged in');
+          return const LandingPage();
+        }
+        else{
+          debugPrint('Invalid username or password');
+          return const LoginScreen();
+        }
+      },
+    );
+
+    
+  }
+  
+ 
+}
+
