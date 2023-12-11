ff4dd2c0e0a7a5b21d3f721431c85e7d4b3cdd00
Simone88kaur <143977991+Simone88kaur@users.noreply.github.com>
2023-12-11 13:16:06 +1100

    Update main.dart

--- whistlingwoodz/lib/main.dart
+++ whistlingwoodz/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:whistlingwoodz/screens/landing_page_screen.dart';
 import 'package:whistlingwoodz/screens/login_screen.dart';
@@ -11,7 +11,7 @@ void main() async {
   
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp( MaterialApp(
+  runApp( MyApp(
     
       debugShowCheckedModeBanner: false,
       title: 'Whistlingwoodz',
