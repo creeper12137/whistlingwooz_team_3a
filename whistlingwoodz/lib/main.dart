1c6bb96a26103dfc7ccf07c8dd199bb2629919fb
Adam McHugh <20226025@students.latrobe.edu.au>
2023-12-11 20:11:19 +1100

    new login screen and fix main dart
    firebase

--- whistlingwoodz/lib/main.dart
+++ whistlingwoodz/lib/main.dart
@@ -1,57 +1,38 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:whistlingwoodz/screens/landing_page_screen.dart';
-import 'package:whistlingwoodz/screens/login_screen.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:whistlingwoodz/screens/wedding_celebrations_screen.dart';
 
-
-
 void main() async {
-  
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp( MyApp(
-    
-      debugShowCheckedModeBanner: false,
-      title: 'Whistlingwoodz',
-      theme: ThemeData(
-        primarySwatch: Colors.amber,
-      ),
-      home: const MyApp(),
-    )
-    );
- 
+  runApp(MaterialApp(
+    navigatorKey: navigatorKey,
+    debugShowCheckedModeBanner: false,
+    title: 'Whistlingwoodz',
+    theme: ThemeData(
+      primarySwatch: Colors.amber,
+    ),
+    home: const MyApp(),
+  ));
 }
+
 final navigatorKey = GlobalKey<NavigatorState>();
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  
-  @override
-  Widget build(BuildContext context)  {  
-    MaterialApp(
-      navigatorKey: navigatorKey,
-      debugShowCheckedModeBanner: false,
-      title: 'Whistlingwoodz',
-      theme: ThemeData(
-        primarySwatch: Colors.amber,
-      ),
-      home: const Wedding(data: false),
-    );
 
-   return  StreamBuilder<User?>(
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
       stream: FirebaseAuth.instance.authStateChanges(),
-      builder: (context, snapshot){
-        if(snapshot.hasData){
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
           return const Wedding(data: true);
-        }
-        else{
+        } else {
           return const Wedding(data: false);
         }
       },
     );
   }
-  
- 
 }
-
