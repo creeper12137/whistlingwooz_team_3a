0c3c429a01e8dd1df5f77be6bd856f0bba6881e9
Simone88kaur <143977991+Simone88kaur@users.noreply.github.com>
2024-01-06 12:34:40 +1100

    Update wedding_celebrations_screen.dart

--- whistlingwoodz/lib/screens/wedding_celebrations_screen.dart
+++ whistlingwoodz/lib/screens/wedding_celebrations_screen.dart
@@ -1,29 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:whistlingwoodz/main.dart';
+import 'package:whistlingwoodz/models/wedding.dart'; // Import the updated Wedding class
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 class Wedding extends StatefulWidget {
-  const Wedding({super.key, required this.data});
+  const Wedding({Key? key, required this.data}) : super(key: key);
   final bool data;
 
   @override
-  State<Wedding> createState() => _WeddingState();
+  _WeddingState createState() => _WeddingState();
 }
 
 class _WeddingState extends State<Wedding> {
-  submitFunction() {
-    runApp(MaterialApp(
-      navigatorKey: navigatorKey,
-      debugShowCheckedModeBanner: false,
-      title: 'Whistlingwoodz',
-      theme: ThemeData(
-        primarySwatch: Colors.amber,
-      ),
-      // ignore: prefer_const_constructors
-      home: MyApp(selectedIndex: 6),
-    ));
-  }
+  // Initialize a Wedding object
+  Wedding newWedding = Wedding(0, "", "", "", "", 0, "", "", "");
 
-  // list variables for drop down menus
+  // list variables for drop-down menus
   final List<String> _themeList = ["Classic", "Contemporary", "Customized"];
   final List<String> _functionList = [
     "Haldi - Mehndi",
@@ -32,8 +24,8 @@ class _WeddingState extends State<Wedding> {
     "Reception"
   ];
   final List<String> _venueList = [
-    "Hyatt Place Melbounre",
-    "Hyatt Place Carribean Park",
+    "Hyatt Place Melbourne",
+    "Hyatt Place Caribbean Park",
     "Grand Hyatt Melbourne",
     "Park Hyatt Melbourne",
     "Hyatt Centric Melbourne",
@@ -47,21 +39,118 @@ class _WeddingState extends State<Wedding> {
     "Other"
   ];
 
-  // initial values for drop down menus
+  // initial values for drop-down menus
   String? _selectedTheme = "Classic";
   String? _selectedFunction = "Haldi - Mehndi";
-  String? _selectedVenue = "Hyatt Place Melbounre";
+  String? _selectedVenue = "Hyatt Place Melbourne";
   String? _selectedBudget = r"$20,000 - $29,999";
+
+  // submit button
+  Widget _buildSubmit() => SizedBox(
+        width: double.infinity,
+        child: OutlinedButton(
+          style: TextButton.styleFrom(
+            padding: const EdgeInsets.symmetric(vertical: 26),
+            foregroundColor: Colors.yellowAccent,
+            backgroundColor: Colors.yellow[900],
+            elevation: 15,
+            shadowColor: Colors.grey,
+            shape: const StadiumBorder(),
+          ),
+          onPressed: () {
+            // Set values to the newWedding object
+            newWedding.theme = _selectedTheme ?? "";
+            newWedding.function = _selectedFunction ?? "";
+            newWedding.venue = _selectedVenue ?? "";
+            newWedding.guestNo = int.parse(_guestController.text);
+            newWedding.budget = _selectedBudget ?? "";
+            newWedding.email = _emailController.text;
+            newWedding.phoneNo = _phoneController.text;
+
+            // Call the submitEvent method from Wedding class
+            newWedding.submitEvent();
+
+            // Navigate to a different screen or perform other actions as needed
+            // For example, you can use Navigator to push a new screen
+            // Navigator.push(context, MaterialPageRoute(builder: (context) => NextScreen()));
+          },
+          child: Text(
+            "Submit Form".toUpperCase(),
+            style: const TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      );
+
+  final TextEditingController _guestController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _phoneController = TextEditingController();
+
+  // guest text form field
+  Widget _buildGuest() => TextFormField(
+        controller: _guestController,
+        keyboardType: TextInputType.number,
+        decoration: const InputDecoration(
+          labelText: "Number of Guests*",
+          prefixIcon: Icon(
+            Icons.group,
+            color: Colors.deepOrangeAccent,
+          ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(10),
+            ),
+          ),
+        ),
+      );
+
+  // email text form field
+  Widget _buildEmail() => TextFormField(
+        controller: _emailController,
+        keyboardType: TextInputType.emailAddress,
+        decoration: const InputDecoration(
+          labelText: "Email*",
+          prefixIcon: Icon(
+            Icons.email,
+            color: Colors.deepOrangeAccent,
+          ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(10),
+            ),
+          ),
+        ),
+      );
+
+  // phone text form field
+  Widget _buildPhone() => TextFormField(
+        controller: _phoneController,
+        keyboardType: TextInputType.phone,
+        decoration: const InputDecoration(
+          labelText: "Phone*",
+          prefixIcon: Icon(
+            Icons.phone,
+            color: Colors.deepOrangeAccent,
+          ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(10),
+            ),
+          ),
+        ),
+      );
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: const AppBarPage(data: widget.data),
       resizeToAvoidBottomInset: false,
       body: Container(
         width: double.infinity,
         height: double.infinity,
-        // backgound photo for landing page
         decoration: const BoxDecoration(
           image: DecorationImage(
             image: AssetImage("assets/images/weddingPage.png"),
@@ -69,7 +158,6 @@ class _WeddingState extends State<Wedding> {
             fit: BoxFit.cover,
           ),
         ),
-        // The box for the drop down menu section
         child: Padding(
           padding: const EdgeInsets.fromLTRB(30, 30, 30, 0),
           child: SingleChildScrollView(
@@ -85,15 +173,12 @@ class _WeddingState extends State<Wedding> {
                   Expanded(
                     child: Column(
                       children: [
-                        // tab title
                         const SizedBox(
                           height: 30,
                         ),
                         const Text(
                           'WEDDING',
                           style: TextStyle(
-                            // color: Colors.black,
-                            // color: Color(0xffFFD700),
                             fontSize: 18,
                             fontWeight: FontWeight.bold,
                             letterSpacing: 2,
@@ -106,17 +191,14 @@ class _WeddingState extends State<Wedding> {
                               child: Column(
                                 crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
-                                  // Theme drop down menu
                                   _buildTheme(),
                                   const SizedBox(
                                     height: 20,
                                   ),
-                                  // Functions drop down menu
                                   _buildFunction(),
                                   const SizedBox(
                                     height: 20,
                                   ),
-                                  // Venue drop down menu
                                   if (_selectedVenue != _venueList[6])
                                     _buildVenue(),
                                   if (_selectedVenue == _venueList[6])
@@ -142,7 +224,6 @@ class _WeddingState extends State<Wedding> {
                                   const SizedBox(
                                     height: 20,
                                   ),
-                                  // Budget drop down menu
                                   if (_selectedBudget != _budgetList[3])
                                     _buildBudget(),
                                   if (_selectedBudget == _budgetList[3])
@@ -165,17 +246,14 @@ class _WeddingState extends State<Wedding> {
                                   const SizedBox(
                                     height: 20,
                                   ),
-                                  // email text form field
                                   _buildEmail(),
                                   const SizedBox(
                                     height: 20,
                                   ),
-                                  // phone text form field
                                   _buildPhone(),
                                   const SizedBox(
                                     height: 30,
                                   ),
-                                  // submit button
                                   _buildSubmit(),
                                 ],
                               ),
@@ -194,7 +272,7 @@ class _WeddingState extends State<Wedding> {
     );
   }
 
-  // Theme drop down menu
+  // Theme drop-down menu
   Widget _buildTheme() => DropdownButtonFormField(
         value: _selectedTheme,
         items: _themeList
@@ -222,7 +300,6 @@ class _WeddingState extends State<Wedding> {
             Icons.subject,
             color: Colors.deepOrangeAccent,
           ),
-          // Todo: check UnderlineInputBorder() option
           border: OutlineInputBorder(
             borderRadius: BorderRadius.all(
               Radius.circular(10),
@@ -231,7 +308,7 @@ class _WeddingState extends State<Wedding> {
         ),
       );
 
-  // Functions drop down menu
+  // Function drop-down menu
   Widget _buildFunction() => DropdownButtonFormField(
         value: _selectedFunction,
         items: _functionList
@@ -254,12 +331,11 @@ class _WeddingState extends State<Wedding> {
         ),
         dropdownColor: Colors.yellow.shade50,
         decoration: const InputDecoration(
-          labelText: "Select Functions*",
+          labelText: "Select Function*",
           prefixIcon: Icon(
             Icons.book_online,
             color: Colors.deepOrangeAccent,
           ),
-          // Todo: check UnderlineInputBorder() option
           border: OutlineInputBorder(
             borderRadius: BorderRadius.all(
               Radius.circular(10),
@@ -268,7 +344,7 @@ class _WeddingState extends State<Wedding> {
         ),
       );
 
-  // Venue drop down menu
+  // Venue drop-down menu
   Widget _buildVenue() => DropdownButtonFormField(
         value: _selectedVenue,
         items: _venueList
@@ -296,7 +372,6 @@ class _WeddingState extends State<Wedding> {
             Icons.place,
             color: Colors.deepOrangeAccent,
           ),
-          // Todo: check UnderlineInputBorder() option
           border: OutlineInputBorder(
             borderRadius: BorderRadius.all(
               Radius.circular(10),
@@ -305,25 +380,7 @@ class _WeddingState extends State<Wedding> {
         ),
       );
 
-  // guest text form field
-  Widget _buildGuest() => TextFormField(
-        autofocus: false,
-        keyboardType: TextInputType.number,
-        decoration: const InputDecoration(
-          labelText: "Number of Guests*",
-          prefixIcon: Icon(
-            Icons.group,
-            color: Colors.deepOrangeAccent,
-          ),
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.all(
-              Radius.circular(10),
-            ),
-          ),
-        ),
-      );
-
-  // Budget drop down menu
+  // Budget drop-down menu
   Widget _buildBudget() => DropdownButtonFormField(
         value: _selectedBudget,
         items: _budgetList
@@ -351,75 +408,15 @@ class _WeddingState extends State<Wedding> {
             Icons.attach_money,
             color: Colors.deepOrangeAccent,
           ),
-          // Todo: check UnderlineInputBorder() option
           border: OutlineInputBorder(
             borderRadius: BorderRadius.all(
               Radius.circular(10),
-            ),
-          ),
-        ),
-      );
-
-  // email text form field
-  Widget _buildEmail() => TextFormField(
-        autofocus: false,
-        keyboardType: TextInputType.emailAddress,
-        decoration: const InputDecoration(
-          labelText: "Email*",
-          prefixIcon: Icon(
-            Icons.email,
-            color: Colors.deepOrangeAccent,
-          ),
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.all(
-              Radius.circular(10),
-            ),
-          ),
-        ),
-      );
-
-  // phone text form field
-  Widget _buildPhone() => TextFormField(
-        autofocus: false,
-        keyboardType: TextInputType.phone,
-        decoration: const InputDecoration(
-          labelText: "Phone*",
-          prefixIcon: Icon(
-            Icons.phone,
-            color: Colors.deepOrangeAccent,
-          ),
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.all(
-              Radius.circular(10),
-            ),
-          ),
-        ),
-      );
-
-  // submit button
-  Widget _buildSubmit() => SizedBox(
-        width: double.infinity,
-        child: OutlinedButton(
-          style: TextButton.styleFrom(
-            padding: const EdgeInsets.symmetric(vertical: 26),
-            foregroundColor: Colors.yellowAccent,
-            backgroundColor: Colors.yellow[900],
-            elevation: 15,
-            shadowColor: Colors.grey,
-            shape: const StadiumBorder(),
-          ),
-          onPressed: () {
-            submitFunction();
-          },
-          child: Text(
-            "Submit Form".toUpperCase(),
-            style: const TextStyle(
-              fontSize: 14,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2,
-              color: Colors.white,
             ),
           ),
         ),
       );
 }
+
+
+
+           
